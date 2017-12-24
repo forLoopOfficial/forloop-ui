6fb3b3b862f935da134836d332e8334669c99a95
Wale Martins <madarasage@gmail.com>
2017-12-24 16:18:57 +0100

    add sponsor

---
+++ components/admin/sponsors/Sponsor.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-card :title="sponsor.name"
+        :img-src="sponsor.image_url"
+        :img-alt="sponsor.name"
+        img-top
+        tag="article"
+        style="width: 10rem;"
+        class="mb-2">
+    <b-button href="#" variant="primary">Edit</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'Sponsor',
+  // lifecycle methods
+
+  props: ['sponsor'],
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
